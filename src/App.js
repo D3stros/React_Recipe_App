282926c33a9d99bede0ba0c05792a5c49c4045dd
Destros <patrick.fallmann@hotmail.com>
2019-09-17 21:53:52 +0200

    Added a counter method to increment the state and also added useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "a6c8cfff";
   const APP_KEY = "9e345bee2df690d6b49996e6fac9609c";
-
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  });
 
   return (
     <div className="App">
@@ -15,6 +20,7 @@ const App = () => {
           Search
         </button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 };

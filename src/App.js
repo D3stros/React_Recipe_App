cfe779ef3524ec7f3ac2e33eb2952f885cc50011
Destros <patrick.fallmann@hotmail.com>
2019-09-17 20:34:10 +0200

    Established APP_ID and APP_Key and an example request to the edamam API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const APP_ID = "a6c8cfff";
+  const APP_KEY = "9e345bee2df690d6b49996e6fac9609c";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <h1></h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
